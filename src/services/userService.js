fd2d7784e722761cbb5ebcefbeefd0b9577f8f39
juandejunin <juandejunin75@gmail.com>
2023-09-15 18:31:58 +0200

    feat:login

--- src/services/userService.js
+++ src/services/userService.js
@@ -17,7 +17,7 @@ const SALT_ROUNDS = 10
 const MSG_NO_VERIFICADO = 'You must verify the account. Check your mail'
 
 // Función para registrar un usuario.
-const registroUsuario = async (req) => {
+const registerUser = async (req) => {
   let data = null
 
   // Valida los datos del usuario utilizando las reglas definidas en validationResult.
@@ -59,5 +59,121 @@ const registroUsuario = async (req) => {
   return createResponse(true, data, null, 201) // Respuesta de éxito.
 }
 
-// Exporta la función registroUsuario para su uso en otros archivos.
-module.exports = { registroUsuario }
+/**
+ * Función para iniciar sesión de un usuario.
+ *
+ * @param {Object} req - Objeto de solicitud HTTP que contiene los datos de inicio de sesión.
+ * @returns {Object} - Objeto de respuesta que indica el resultado de la operación.
+ */
+
+const loginUser = async (req) => {
+  // Paso 1: Validar los errores de la solicitud
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return createResponse(false, null, errors.array(), 400);
+  }
+
+  // Paso 2: Obtener el email y la contraseña de la solicitud
+  const { email, password } = req.body;
+  
+  
+  // Paso 3: Buscar al usuario en la base de datos por su email
+  const userDB = await User.find({ email });
+  
+
+  if (!password || !userDB) {
+    return createResponse(false, null, 'Invalid email or password', 401);
+  }
+
+  // Paso 4: Verificar si se encontró un usuario en la base de datos
+  if (userDB) {
+    // Paso 5: Comprobar si se solicitó un cambio de contraseña para el usuario
+    if (userDB.security?.restorePassword) {
+      return createResponse(
+        false,
+        null,
+        'A password change has been requested and you must complete the process',
+        400
+      );
+    }
+
+    // Paso 6: Comparar la contraseña proporcionada con la contraseña almacenada
+    if (!bcrypt.compareSync(password, userDB[0].password)) {
+      return createResponse(false, null, 'Invalid email or password', 401);
+    }
+
+    // Paso 7: Verificar si la cuenta del usuario está verificada
+    if (!userDB[0].security?.verified) {
+      return createResponse(false, null, MSG_NO_VERIFICADO, 400);
+    }
+
+    // Paso 8: Generar un token de autenticación para el usuario
+    const userToken = {
+      id: userDB._id,
+      name: userDB.name,
+    };
+    const token = signToken(userToken);
+
+    // Paso 9: Preparar los datos de respuesta
+    const data = {
+      id: userDB._id,
+      name: userDB.name,
+      username: userDB.username,
+      token,
+      
+    };
+
+    // Paso 10: Devolver una respuesta exitosa con los datos del usuario
+    return createResponse(true, data, null, 200);
+  }
+
+  // Paso 11: Devolver una respuesta de error si no se encontró un usuario en la base de datos
+  return createResponse(false, null, 'Invalid email or password', 401);
+};
+
+/**
+ * Función para verificar la dirección de correo electrónico de un usuario mediante un token criptográfico.
+ *
+ * @param {Object} req - Objeto de solicitud HTTP que contiene el token criptográfico en los parámetros.
+ * @returns {Object} - Objeto de respuesta que indica el resultado de la operación de verificación.
+ */
+const verifyEmail = async (req) => {
+  let data = null;
+
+  // Paso 1: Obtener el token criptográfico de los parámetros de la solicitud
+  const { cryptoToken } = req.params;
+
+  // Paso 2: Buscar al usuario en la base de datos por el token criptográfico
+  const userExists = await User.find({ 'security.cryptoToken': cryptoToken });
+
+  // Paso 3: Verificar si se encontró un usuario con el token criptográfico
+  if (!userExists) {
+    return createResponse(false, data, USER_ERROR, 400);
+  }
+
+  // Paso 4: Realizar la verificación de la dirección de correo electrónico del usuario
+  userExists.security = verificarUser(userExists);
+
+  // Paso 5: Actualizar la información del usuario en la base de datos
+  const userUpdated = await User.findOneAndUpdate(
+    { 'security.cryptoToken': cryptoToken }, // Condición de búsqueda
+    { $set: { 'security.verified': true } }, // Actualización
+    { new: true } // Opciones para devolver el documento actualizado
+  );
+  
+
+  // Paso 6: Preparar los datos de respuesta con la información actualizada del usuario
+  data = {
+    email: userUpdated.email,
+    username: userUpdated.username,
+    verified: userUpdated.security.verified,
+  };
+
+  // Paso 7: Devolver una respuesta exitosa con los datos de usuario verificados
+  return createResponse(true, data, null, 200);
+};
+
+
+
+// Exporta la funciónes para su uso en otros archivos.
+module.exports = { registerUser, loginUser, verifyEmail }
