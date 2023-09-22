269e77beb31ec9d094da60098f8f3ba574a5dbe3
juandejunin <juandejunin75@gmail.com>
2023-09-22 16:51:02 +0200

    fix: controllers user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,60 +1,67 @@
-const userService = require('../services/userService')
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('../utils/jwt')
+const { validationResult } = require('express-validator')
+const { createResponse } = require('../utils/responseGenerator')
+const { initUserSeguridad, verificarUser, buildForgotPassword, passwordReset } = require('../utils/verificationManager')
+const { sendVerificationMail, sendForgotPasswordMail, sendChangedPasswordMail } = require('../utils/emailTransporter')
+const buildHostName = require('../utils/hostManager')
 
+// Mensaje de error estándar en caso de problemas al obtener un usuario.
+const USER_ERROR = 'Error getting user'
+
+// Número de rondas de sal para el algoritmo de hashing de contraseñas.
+const SALT_ROUNDS = 10
+
+// Mensaje para usuarios no verificados.
+const MSG_NO_VERIFICADO = 'You must verify the account. Check your mail'
+
+// Función para registrar un usuario.
 const registerUser = async (req, res) => {
-  try {
-    // Recoger datos de la petición
-    const params = req.body;
+  let data = null
 
-    // Comprobar que se envían todos los datos obligatorios
-    if (!params.username || !params.email || !params.password || !params.nick) {
-      return res.status(400).json({
-        status: "error",
-        message: "Faltan datos que son obligatorios",
-      });
-    }
-
-    // Control de usuarios duplicados
-    const existingUser = await User.findOne({
-      $or: [
-        { email: params.email.toLowerCase() },
-        { nick: params.nick.toLowerCase() },
-      ],
-    });
-
-    if (existingUser) {
-      return res.status(200).json({
-        status: "success",
-        message: "El usuario ya existe",
-      });
-    }
-
-    // Cifrar la contraseña
-    const hashedPassword = await bcrypt.hash(params.password, 10);
-    params.password = hashedPassword;
-
-    // Guardar información del nuevo usuario
-    const newUser = new User(params);
-
-    // Guardar en la base de datos
-    await newUser.save();
-
-    // Devolver el resultado
-    return res.status(200).json({
-      status: "success",
-      message: "Usuario registrado exitosamente",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      status: "error",
-      message: "Error en el servidor",
-    });
+  // Valida los datos del usuario utilizando las reglas definidas en validationResult.
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return createResponse(false, data, errors.array(), 400)
   }
-};
 
+  // Extrae los campos del cuerpo de la solicitud.
+  const { username, email, password } = req.body
+
+  if (!username || !email || !password) {
+    return createResponse(false, null, 'The username, email and password fields are required', 401);
+  }
+
+  // Verifica si ya existe un usuario con el mismo nombre de usuario o correo electrónico.
+  const usernameExists = await User.findOne({ username })
+  const emailExists = await User.findOne({ email })
+
+  if (usernameExists || emailExists) {
+    return createResponse(false, data, 'Invalid Email/Username', 400)
+  }
+
+  // Genera un hash de la contraseña antes de almacenarla en la base de datos.
+  const passwordHash = await bcrypt.hash(password, SALT_ROUNDS)
+
+  // Crea un objeto userData con los datos del usuario y la contraseña hash.
+  const userData = req.body
+  userData.password = passwordHash
+  userData.security = initUserSeguridad() // Inicializa la seguridad del usuario.
+
+  // Crea un nuevo usuario en la base de datos.
+  const createdUser = await User.create(userData)
+
+  // Envía un correo de verificación al usuario recién registrado.
+  await sendVerificationMail(createdUser, buildHostName(req))
+
+  data = {
+    msg: 'Registered successfully. ' + MSG_NO_VERIFICADO,
+    id: createdUser._id
+  }
+
+  return createResponse(true, data, null, 201) // Respuesta de éxito.
+}
 
 const loginUser = async (req, res) => {
   // Recoger parametros que lleguen en la peticion
@@ -63,7 +70,7 @@ const loginUser = async (req, res) => {
   if (!params.email || !params.password) {
     return res.status(404).send({
       status: "error",
-      message: "faltan datos por enviar"
+      message: "missing data to send"
     });
   }
 
@@ -74,7 +81,7 @@ const loginUser = async (req, res) => {
     if (!user) {
       return res.status(404).send({
         status: "error",
-        message: "No se encuentra el usuario"
+        message: "User not found"
       });
     }
 
@@ -84,7 +91,7 @@ const loginUser = async (req, res) => {
     if (!pwd) {
       return res.status(400).send({
         status: "error",
-        message: "No te identificaste correctamente"
+        message: "You did not identify yourself correctly"
       });
     }
 
@@ -94,7 +101,7 @@ const loginUser = async (req, res) => {
     // Devolver datos de usuario
     return res.status(200).send({
       status: "success",
-      message: "accion de login",
+      message: "login action",
       user: {
         id: user._id,
         name: user.name,
@@ -106,23 +113,53 @@ const loginUser = async (req, res) => {
     console.error(error);
     return res.status(500).send({
       status: "error",
-      message: "Error en el servidor"
+      message: "Server error"
     });
   }
 };
 
 
-const verifyEmail = async (req, res, next) => {
-  try {
-    const { success } = await userService.verifyEmail(req)
-    if (success === true) {
-      res.redirect(process.env.URL_SIGNIN)
-    } else {
-      res.redirect(process.env.URL_REGISTER)
-    }
-  } catch (error) {
-    next(error)
+/**
+ * Función para verificar la dirección de correo electrónico de un usuario mediante un token criptográfico.
+ *
+ * @param {Object} req - Objeto de solicitud HTTP que contiene el token criptográfico en los parámetros.
+ * @returns {Object} - Objeto de respuesta que indica el resultado de la operación de verificación.
+ */
+
+const verifyEmail = async (req) => {
+  let data = null;
+
+  // Paso 1: Obtener el token criptográfico de los parámetros de la solicitud
+  const { cryptoToken } = req.params;
+
+  // Paso 2: Buscar al usuario en la base de datos por el token criptográfico
+  const userExists = await User.find({ 'security.cryptoToken': cryptoToken });
+
+  // Paso 3: Verificar si se encontró un usuario con el token criptográfico
+  if (!userExists) {
+    return createResponse(false, data, USER_ERROR, 400);
   }
-}
+
+  // Paso 4: Realizar la verificación de la dirección de correo electrónico del usuario
+  userExists.security = verificarUser(userExists);
+
+  // Paso 5: Actualizar la información del usuario en la base de datos
+  const userUpdated = await User.findOneAndUpdate(
+    { 'security.cryptoToken': cryptoToken }, // Condición de búsqueda
+    { $set: { 'security.verified': true } }, // Actualización
+    { new: true } // Opciones para devolver el documento actualizado
+  );
+  
+  // Paso 6: Preparar los datos de respuesta con la información actualizada del usuario
+  data = {
+    email: userUpdated.email,
+    username: userUpdated.username,
+    verified: userUpdated.security.verified,
+  };
+
+  // Paso 7: Devolver una respuesta exitosa con los datos de usuario verificados
+  return createResponse(true, data, null, 200);
+};
+
 
 module.exports = {   registerUser, loginUser, verifyEmail }
